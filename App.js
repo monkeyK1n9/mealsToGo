881ca5583542732da55d25620f391a76e96b2287
monkeyKing <guillaumedjio@gmail.com>
2021-12-01 08:41:16 +0100

    Search and content sections

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
@@ -5,7 +6,12 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View>
+        <Text>Search</Text>
+      </View>
+      <View>
+        <Text>App content</Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
